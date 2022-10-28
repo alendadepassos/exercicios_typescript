1f9a4aec1f98566806c4dfd792c0e6e13d6be54d
lucas.s.passos <lucas.s.passos@stf.jus.br>
2022-10-27 21:02:53 -0300

    correção de identação

--- src/containsduplicate.ts
+++ src/containsduplicate.ts
@@ -1,12 +1,10 @@
 function containsDuplicate(nums: number[]): boolean {
-    let duplicate : boolean;
+    let duplicate = false;
     for (let i = 0; i < nums.length; i++) {
-        for (let index = 1; index < nums.length; index++) {
+        for (let index = i + 1; index < nums.length; index++) {
             if (nums[i] === nums[index]) {
                 duplicate = true;
                 break;
-            } else {
-                duplicate = false;
             }
         }
     }
--- src/plusone.ts
+++ src/plusone.ts
@@ -1,16 +1,16 @@
 function plusOne(digits: number[]): number[] {
-  for (let index = digits.length - 1; index >= 0; index--) {
-    if (digits[index] < 9) {
-      digits[index] = digits[index] + 1;
-      break;
-    } else {
-      if (index === 0) {
-        digits.unshift(1);
-        digits[1] = 0;
-      } else {
-        digits[index] = 0;
-      }
+    for (let index = digits.length - 1; index >= 0; index--) {
+        if (digits[index] < 9) {
+            digits[index] = digits[index] + 1;
+            break;
+        } else {
+            if (index === 0) {
+                digits.unshift(1);
+                digits[1] = 0;
+            } else {
+                digits[index] = 0;
+            }
+        }
     }
-  }
-  return digits;
+    return digits;
 }
